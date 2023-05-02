cdade86868e5ae817797d9395514aa6649a11f71
Robert Reris <rreris@fortinet.com>
2023-05-02 19:58:08 -0400

    module fixes

--- scripts/generate-site.sh
+++ scripts/generate-site.sh
@@ -12,9 +12,9 @@
 TOP_LEVEL=$(git rev-parse --show-toplevel)
 
 [[ "$#" > 1 ]] && echo "Usage: ./generate-site.sh [output path]" && exit 0
-[[ "$#" < 1 ]] && OUT_PATH=$TOP_LEVEL || OUT_PATH=$1
+[[ "$#" < 1 ]] && OUT_PATH=$TOP_LEVEL || OUT_PATH=$(pwd)
 
 docker build -t app-image:latest --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" -f DockerfileBuild .
 DOCK_CONT=$(docker run -d app-image:latest)
-docker cp $DOCK_CONT:/home/app/CentralRepoTest/public $OUT_PATH
+docker cp $DOCK_CONT:/home/CentralRepoTest/public $OUT_PATH
 docker rm -f $DOCK_CONT
