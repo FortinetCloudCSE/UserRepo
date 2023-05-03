63dd9cd30cc3f486ba4a81baed8a42429391241c
Jeff Kopko <jkopko@gmail.com>
2023-05-03 15:00:12 -0700

    Readme update & mods for MacM1 (ssh keys + arm vs amd Hugo.deb)

--- scripts/generate-site.sh
+++ scripts/generate-site.sh
@@ -14,7 +14,7 @@ TOP_LEVEL=$(git rev-parse --show-toplevel)
 [[ "$#" > 1 ]] && echo "Usage: ./generate-site.sh [output path]" && exit 0
 [[ "$#" < 1 ]] && OUT_PATH=$TOP_LEVEL || OUT_PATH=$(pwd)
 
-docker build -t app-image:latest --build-arg ssh_prv_key="$(cat ~/.ssh/id_ed25519)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_ed25519.pub)" -f DockerfileTest .
+docker build -t app-image:latest --build-arg ssh_prv_key="$(cat ~/.ssh/id_ed25519)" --build-arg ssh_pub_key="$(cat ~/.ssh/id_ed25519.pub)" -f DockerfileBuild .
 DOCK_CONT=$(docker run -d app-image:latest)
 docker cp $DOCK_CONT:/home/CentralRepoTest/public $OUT_PATH
 docker rm -f $DOCK_CONT
